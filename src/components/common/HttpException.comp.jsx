8e86e5e040a1e4bf4cb8e6dab71ec3e3a2de92c0
bb2002 <5252bb@daum.net>
2022-04-03 20:37:38 +0900

    [ADD]: HttpException 처리 완료

---
+++ src/components/common/HttpException.comp.jsx
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react';
+import Title from "antd/es/typography/Title";
+
+const HttpExceptionComp = ({ code, customTitle, customSubTitle }) => {
+  const [title, setTitle] = useState('알 수 없는 오류')
+  const [subTitle, setSubTitle] = useState('알 수 없는 오류가 발생했습니다. 개발팀에 문의 하십시오.');
+
+  useEffect(() => {
+    if (customTitle || customSubTitle) {
+      setTitle(customTitle);
+      setSubTitle(customSubTitle)
+    } else {
+      switch (code) {
+        case -1:
+          setTitle('서버에 요청 할 수 없음');
+          setSubTitle('서버에 요청을 보낼 수 없습니다. 네트워크 상태를 확인하십시오.')
+          break;
+        case 400:
+          setTitle('요청이 잘못 됨');
+          setSubTitle('잘못된 요청을 보냈습니다. 메인으로 이동하십시오.')
+          break;
+        case 403:
+          setTitle('접근 거부');
+          setSubTitle('접근 권한이 없습니다. 메인으로 이동하십시오.')
+          break;
+        case 404:
+          setTitle('파일을 찾을 수 없음');
+          setSubTitle('파일이 존재하지 않습니다. 다운로드 경로를 다시 확인하시기 바랍니다.')
+          break;
+        case 500:
+          setTitle('내부 서버 오류');
+          setSubTitle('서버 장애가 발생했습니다. 나중에 다시 시도하거나 오류가 지속되면 개발팀에 문의하십시오.')
+          break;
+      }
+    }
+
+  }, [code, customTitle, customSubTitle])
+
+
+
+  return (
+    <div>
+      <Title level={2}>{title}</Title>
+      <p style={{ color: '#606060' }}>{subTitle}</p>
+    </div>
+  );
+};
+
+export default HttpExceptionComp;
--- src/pages/Download.page.jsx
+++ src/pages/Download.page.jsx
@@ -4,6 +4,7 @@ import {useParams} from "react-router-dom";
 import "../styles/pages/DownloadPage.css"
 import DownloadComp from "../components/download/Download.comp";
 import {LoadingOutlined} from "@ant-design/icons";
+import HttpExceptionComp from "../components/common/HttpException.comp";
 
 const DownloadPage = () => {
   const { easyUUID } = useParams()
@@ -16,6 +17,7 @@ const DownloadPage = () => {
   return (
     <div className="download-page">
       {
+        // 로딩 중인 경우
         post.loading && (
           <div style={{ height: 150 }}>
             <LoadingOutlined style={{ fontSize: '2rem' }}/>
@@ -23,12 +25,21 @@ const DownloadPage = () => {
         )
       }
       {
+        // 포스트 데이터를 성공적으로 받은 경우
         (!post.loading && post.data) && (
         <DownloadComp
           isLocked={post.data.authMethod !== 'open'}
           currentDownloadCount={post.data.currentDownloadCount}
           files={post.data.files}
           expiredAt={post.data.expiredAt} />
+        )
+      }
+      {
+        // HTTP 오류가 발생한 경우
+        (post.error && post.code !== 0) && (
+          <div style={{ height: 150 }}>
+            <HttpExceptionComp code={post.code} />
+          </div>
         )
       }
 
