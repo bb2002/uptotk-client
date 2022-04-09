81c2565b42a24d9ac3e53860e26ecc63e0b5b30b
ballbot3451 <ballbot@cashwalk.io>
2022-04-09 21:42:20 +0900

    [FIX]: production 상수 변경

--- src/libs/axios/index.js
+++ src/libs/axios/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const UPTOTK_SERVER = process.env.NODE_ENV === 'production' ? 'https://upto.tk' : 'http://localhost:3001';
+export const UPTOTK_SERVER = process.env.NODE_ENV === 'production' ? 'https://api.upto.tk' : 'http://localhost:3001';
 
 export function generateAxios() {
   const options = {
--- src/libs/const.js
+++ src/libs/const.js
@@ -1,1 +1,1 @@
-export const UPTOTK_SERVER = 'http://localhost:3001'
+export const UPTOTK_SERVER = 'https://api.upto.tk'
