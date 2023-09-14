322767e9ebb65ca67d52f0708fa9f33f56d37787
Ballbot <5252bb@daum.net>
2023-09-14 00:33:11 +0000

    경고 메시지 추가

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AnalysisComp from "./components/Analysis.comp";
 import DownloadPage from "./pages/Download.page";
 import React, {useEffect, useState} from "react";
 import DevelopersPage from "./pages/Developers.page";
+import { Alert } from 'antd';
 
 
 function App() {
@@ -22,7 +23,14 @@ function App() {
     <div className="app-container">
       <HeaderComp />
       <AnalysisComp />
+
       <div className="container">
+        <br />
+        <Alert
+          message="서비스 점검 안내"
+          description="2023년 9월 14일 오전 9시 30분 부터 오후 4시 30분 까지 (약 7시간) 전기 공사로 인해 일시적으로 서비스가 중단될 예정입니다. 이용에 참고 바랍니다."
+          type="warning"
+        />
         <div className="ad-container">
           {
             (windowWidth > 992) && (
